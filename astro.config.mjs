9810a762152410fbb8c5cc980e6a23f71c443c3a
wjwei-handsome <weiwenjie@westlake.edu.cn>
2024-12-22 21:35:19 +0800

    change astro config for static

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en"],
   },
-  // output: "static",
+  output: "static",
   // adapter: vercel(
   //   {
   //     webAnalytics: {
